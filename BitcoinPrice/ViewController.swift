4038adb3a6dadc05baeaf91956e9224491fe8f88
Vitor Scheffer <vitorscheffer61@gmail.com>
2022-11-02 12:00:45 -0300

    Buscando requisicao com dataTask e formatando retorno Json para dicionario, acessando indice BRL no dicionario de retorno.

--- BitcoinPrice/ViewController.swift
+++ BitcoinPrice/ViewController.swift
@@ -11,7 +11,28 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let baseUrl = URL(string: "https://blockchain.info/ticker") {
+            let tarefa = URLSession.shared.dataTask(with: baseUrl) { dados, requisicao, erro in
+                if erro == nil {
+                    if let dadosRetorno = dados {
+                        do {
+                            if let objetoJson = try JSONSerialization.jsonObject(with: dadosRetorno, options: []) as? [String: Any] {
+                                if let brl = objetoJson["BRL"] {
+                                    print(brl)
+                                }
+                            }
+                        }catch {
+                            print("Erro ao formatar retorno!")
+                        }
+                    }
+                } else {
+                    print("Erro ao realizar tarefa")
+                }
+            }
+            tarefa.resume()
+        }
+        
     }
 
 
